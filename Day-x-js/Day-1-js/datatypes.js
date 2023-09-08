123d0c03a25ebde2f914da03152cc4af5eef289d
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-09-08 11:46:01 +0530

    updates

--- Day-x-js/Day-1-js/datatypes.js
+++ Day-x-js/Day-1-js/datatypes.js
@@ -1,7 +1,22 @@
 // Data Types
 
-// 1. String
+// 1. String : 
 // Syantax = "", '', ``
+
+// 2. Number 
+// Syntax :
+//  69, 70-1, 143, 6969, 6.9,0
+
+// 3. Boolean :
+// true,false
+
+// 4. Undefined
+// initially no value assigned
+
+
+// Variables
+
+// Syntax -->
 
 
 // var myName= "Sameer";
@@ -14,11 +29,11 @@
 // console.log(isFollowPassion)
 
 
-var myName = "Sameer Khadtale";
+var myName = "Sameer";
 var myAge = 24;
 
-var message = "Welcome to our Page Mr. ${myName}";
-console.log(message);
+// var message = "Welcome to our Page Mr. ${myName}";
+// console.log(message);
 
 var newMessage = `Welcome to our Page Mr. ${myName}`;
 console.log(newMessage);
